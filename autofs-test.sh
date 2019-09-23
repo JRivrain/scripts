ecfbcf7b11a8ea6d44031100cb540a95bc221c89
Jonathan Rivrain <43809151+JRivrain@users.noreply.github.com>
2019-09-23 19:28:20 +0200

    Update autofs-test.sh

--- autofs-test.sh
+++ autofs-test.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
+
+if  [ -d /srv/nfs ]; then
+read -p "/srv/nfs found. Wanna do a angerous roll back ? Will wipe all config files and directories !! Y/N" -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]] ;  then
+        exit 1
+    else
+        rm -rf /srv/{smb,nfs}
+        >/etc/exports
+        >/etc/samba/smb.conf
+        >/etc/auto.master
+        >/etc/auto.nfs
+        >/etc/auto.misc
+        rm /root/fakedd
+    fi
+fi
+
 mkdir /srv/{smb,nfs}; chmod 777 /srv/smb
 
+LOG=$1
+[ -z $LOG ] && LOG=/dev/null
+
 nfs_test() {
-set -e
-    systemctl list-unit-files |grep nfsserver &>/dev/null || zypper in -y nfs-kernel-server &>/dev/null
+set -ex
+    systemctl list-unit-files |grep nfsserver || zypper in -y nfs-kernel-server
+    echo "/srv/nfs *(fsid=0,rw,no_root_squash,sync,no_subtree_check)" >>/etc/exports
     echo "/nfs /etc/auto.nfs --timeout=10" >>/etc/auto.master
     echo "localnfs -fstype=nfs localhost:/srv/nfs" >> /etc/auto.nfs
     systemctl restart autofs nfsserver
     touch /nfs/localnfs/testfile
-    ls /srv/nfs/testfile &>/dev/null
+    ls /srv/nfs/testfile
 }
 
 samba_test() {
-set -e
-systemctl list-unit-files |grep smb &>/dev/null || zypper in -y samba &>/dev/null
+set -ex
+systemctl list-unit-files |grep smb || zypper in -y samba
 echo "
 [stuff]
     browseable = yes
@@ -25,23 +45,21 @@ echo "
 echo "/cifs /etc/auto.smb --timeout=10" >> /etc/auto.master
 systemctl restart autofs smb
 touch /cifs/localhost/stuff/testfile
-ls /srv/smb/tes &>/dev/null
+ls /srv/smb/testfile
 }
 
 local_fs() {
-set -e
-    dd if=/dev/zero of=/root/fakedd bs=1M count=50 &>/dev/null
-    mke2fs -t ext2 /root/fakedd &>/dev/null
+set -ex
+    dd if=/dev/zero of=/root/fakedd bs=1M count=50
+    mke2fs -t ext2 /root/fakedd
     echo "/- /etc/auto.misc --timeout=10" >> /etc/auto.master
     echo "/mnt -fstype=ext2 :/root/fakedd" >> /etc/auto.misc
     systemctl restart autofs
-    ls /mnt/lost+found &>/dev/null
+    ls /mnt/lost+found
 }
 
-
-
-nfs_test && echo "NFS test passed" || echo "NFS test failed"
-samba_test && echo "Samba test passed" || echo "Samba test failed"
-local_fs && echo "Local FS test passed" || echo "Local FS test failed"
+nfs_test &>$LOG && echo "NFS test passed" || echo "NFS test failed"
+samba_test &>>$LOG && echo "Samba test passed" || echo "Samba test failed"
+local_fs &>>$LOG && echo "Local FS test passed" || echo "Local FS test failed"
 
 exit 0
