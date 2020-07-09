f0cf3a4f58ca5eec9a795a0e322cb9253b466816
Jonathan Rivrain <43809151+JRivrain@users.noreply.github.com>
2020-07-09 15:07:46 +0200

    Update xmlSimple-example.pl

--- xmlSimple-example.pl
+++ xmlSimple-example.pl
@@ -9,7 +9,6 @@ my $profile = XMLin('./clone-autoinst.xml');
 
 print "name of lv: $profile->{partitioning}->{drive}[0]->{partitions}->{partition}[0]->{lv_name}\n";
 
-my @part;
 foreach my $part (@{$profile->{partitioning}->{drive}[0]->{partitions}->{partition}}) {
 	my $count = 0;
 	print "$part->{lv_name} is mounted on $part->{mount} and has a size of $part->{size}\n";
