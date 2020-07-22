8c307d10d2b7d1f71dfc4031b8f996f2e3969214
Jonathan Rivrain <43809151+JRivrain@users.noreply.github.com>
2020-07-22 11:50:22 +0200

    Update getphysical.pm

--- getphysical.pm
+++ getphysical.pm
@@ -8,7 +8,7 @@ use Data::Dumper;
 
 my @PV = split(/\n/, `pvdisplay |grep "PV Name" |awk '{print \$3}'`);
 my @VG = split(/\n/, `vgdisplay |grep "VG Name" |awk '{print \$3}'`);
-my %PVs_in_VGs;
+my %PEs_in_VGs;
 
 foreach my $vg (@VG) {
 	my @list_of_pvs;
@@ -22,12 +22,12 @@ foreach my $vg (@VG) {
 			chomp($extents_in_pv);
 			push (@extents_in_vg, $extents_in_pv);			
 		}
-		$PVs_in_VGs{$vg} = [@extents_in_vg]
+		$PEs_in_VGs{$vg} = [@extents_in_vg]
 	}
 
 }
 
 $Data::Dumper::Sortkeys = 1; 
-print Dumper %PVs_in_VGs;
+print Dumper %PEs_in_VGs;
 
 1;
