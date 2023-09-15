84a0adfc84ffb0c900b05091bb0b36bb7a2f0b35
Jonathan Rivrain <43809151+JRivrain@users.noreply.github.com>
2023-09-15 18:11:41 +0200

    Update batch-clone.sh
    Add possibility to parse arguments.

--- batch-clone.sh
+++ batch-clone.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Parse args
-if [[ $# != [2] ]] ; then
-echo "Usage: $0 <Github PR or branch> <url with jobs to clone>"
+if [[ $# != [23] ]] ; then
+echo "Usage: $0 \"<Github PR or branch>\" \"<url with jobs to clone>\" \"<optional args>\""
 exit 1
 fi
 
@@ -10,9 +10,10 @@ wget "$2" -O /tmp/job_ids
 job_ids=`grep jobid /tmp/job_ids |awk -F 'data-jobid="' '{print $2}' |cut -d'"' -f1 |xargs`
 PR=$1
 webui=`echo $2 | awk -F"/overview" '{print $1}'`
+args=$3
 
 for jobs in $job_ids; do
-openqa-clone-custom-git-refspec $PR ${webui}/${jobs}
+openqa-clone-custom-git-refspec $PR ${webui}/${jobs} $args
 sleep 1
 done
 
